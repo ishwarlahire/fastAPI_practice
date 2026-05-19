231776c268614f57e45d35ea1291b3ba9bd8f981
ishwarlahire <ishwarlahire2004@gmail.com>
2026-05-19 19:03:23 +0530

    update and add some new routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path,HTTPException
+from fastapi import FastAPI,Path,HTTPException, Query
 import json 
 
 app = FastAPI()
@@ -28,3 +28,19 @@ def view_patient(patient_id:str = Path(..., description='ID from DB', example='P
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404,detail='Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by:str = Query(...,description='sort on the basis of hight ans weight or BMI'),order:str = Query('asc',description='sort in asc or desc orders')):
+    
+    valid_fields = ['height','weight','BMI']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail= f'Invalid field select from {valid_fields}')
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail=f'Invalid order selection between asc and desc')
+    
+    data = load_data()
+    sort_order = True if order=='desc' else False
+    sorted_data = sorted(data.values(),key = lambda x:x.get(sort_by,0))
+
+    return sorted_data
