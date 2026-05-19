d71acd760652054f42dc57b3d1661db4023d48cc
ishwarlahire <ishwarlahire2004@gmail.com>
2026-05-19 11:16:27 +0530

    update and add some new routes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException
+import json 
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def  hello():
@@ -9,3 +15,16 @@ def  hello():
 @app.get("/about")
 def about():
     return{"message":"This is a fast API application"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
+
+@app.get('/patient/{patient_id}')  #using Path Parameter
+def view_patient(patient_id:str = Path(..., description='ID from DB', example='P001')):
+    #load all data
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail='Patient not found')
