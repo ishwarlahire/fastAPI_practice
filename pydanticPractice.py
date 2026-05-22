14ae5c78aa0f27497370b0fce84f4beea2d26530
ishwarlahire <ishwarlahire2004@gmail.com>
2026-05-22 10:14:11 +0530

    practice on pydantic library with data validation and baseModel

--- pydanticPractice.py
+++ pydanticPractice.py
@@ -1,8 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List,Dict,Set,Optional,Annotated
 
 class Patient(BaseModel):
-    name : str
-    age:int
+    name :Annotated[
+        str,
+        Field(max_length=100,title='Name',
+              description='Enter Name of the patient',
+              examples=['Ishwar','Ram'])]
+    email:EmailStr
+    age:int = Field(gt=0,lt=100)
+    linkedin:AnyUrl
+    weight:Annotated[float,Field(gt=0,strict=True,lt=200)]
+    married:Annotated[bool,Field(default=None,description='Select Your married Status')]
+    allergies:Annotated[Optional[List[str]],Field(default=None, max_length=5)]
+    contact_details:dict[str,str]
+
 
 def insert_data(patient:Patient):
     print(patient.name)
@@ -14,7 +26,12 @@ def update_data(patient:Patient):
     print(patient.age)
     print("data updated successfully")
 
-patient_data={'name':"Ishwar",'age':22}
+patient_data={'name':"Ishwar",
+              'email':'ishwarlahire2004@gmail.com',
+              'age':22,
+              'linkedin':'https://www.ishwarlahire.com',
+              'weight':69,
+              'contact_details':{'Phone':'1234567890'}}
 
 patient1 = Patient(**patient_data)
 
