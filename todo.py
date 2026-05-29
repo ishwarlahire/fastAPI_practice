657c2c35817cfdc017f4f0a1225c4a0fb7cbdd8f
ishwarlahire <ishwarlahire2004@gmail.com>
2026-05-29 10:08:03 +0530

    add user model and perform operation user model

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 
 app = FastAPI()
 Todos=[]
@@ -34,3 +34,62 @@ def getDyId(id:int):
                 "data":record
                 }
     return{"message":"record not found"}
+
+
+@app.put("/update")
+def update(id:int,data_update:Todo):
+    for index,data in enumerate(Todos):
+        if data.id == id:
+            Todos[index]=data_update
+            return {
+                "message":"data updated successfully",
+                "data":data_update
+            }
+    return{
+        "error":"record not found"
+    }
+
+@app.delete("/delete")
+def delete(id:int):
+    for index,data in enumerate(Todos):
+        if data.id == id:
+            Todos.pop(index)
+            return{
+                "message":"record delete successfully",
+                "data":Todos
+            }
+    return{
+        "error":"record not found"
+    }
+
+
+ #path , Query , body 
+
+users = []
+class User(BaseModel):
+        name:str
+        age:int
+        email:EmailStr
+
+@app.post("/createUser")
+def createUser(user:User):
+    users.append(user)
+    # users.append(12)
+    return{
+        "message":"user created",
+        "data":user
+    }
+
+@app.put("/updateUser{user_id}")
+def updateUser(user_id:int,user:User,notify:bool = False):
+    if user_id<len(users):
+        users[user_id] = user
+        
+        return{
+            "message":"user updated ",
+            "notify": notify,
+            "data":user
+        }
+    return{
+        "error":"user not found"
+    }
